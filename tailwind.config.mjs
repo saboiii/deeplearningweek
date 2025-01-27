35eb072eabebb526a8824ca59d007323500d74ee
saboiii <saba.x.azad@gmail.com>
2025-01-27 13:17:00 +0800

    ok this better do it

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: false,
+  darkMode: 'class',
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
